f4e294b6e5861f329778ac9ceb0220ddfca55c1b
gabriel <gabrielalcantarabarros524@gmail.com>
2021-06-08 19:15:56 -0300

    refatoraçao ubook init

---
+++ ubook-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HeaderInicial />
+  </div>
+</template>
+
+<script>
+import HeaderInicial from "@/components/HeaderInicial.vue";
+export default {
+  name: "App",
+  components: {
+    HeaderInicial,
+  },
+};
+</script>
+
+<style></style>
---
+++ ubook-app/src/components/HeaderInicial.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div>
+      <img src="../assets/ubook-logo.svg" alt="logotipo ubook" />
+    </div>
+    <input type="text" placeholder="busca" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderInicial",
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  margin: 20px;
+}
+header div {
+  flex: 1;
+}
+
+header input {
+  flex: 3;
+  background: #e4e7f4;
+}
+</style>
